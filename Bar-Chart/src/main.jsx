34d5c52d588dd3574eac89ccada40866e101f9b8
Jax-Man <amcli8079@gmail.com>
2025-03-16 20:46:35 -0400

    basic Framework

---
+++ Bar-Chart/src/main.jsx
@@ -0,0 +1,19 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux'; 
+import './index.css';
+import App from './App.jsx';
+
+const store = configureStore({
+  reducer: {
+    
+  }
+  });
+
+createRoot(document.getElementById('root')).render(
+
+  <Provider store={store}>
+    <App />
+  </Provider>,
+)
